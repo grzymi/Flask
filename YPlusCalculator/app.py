c356d8485e667ed086a533b81740f342e6310bd3
grzymi <przemyslaw.grzymislawski@gmail.com>
2019-04-04 21:35:57 +0200

    update

--- YPlusCalculator/app.py
+++ YPlusCalculator/app.py
@@ -6,6 +6,7 @@
 
 from flask import Flask, request, render_template
 from math import sqrt
+import y_plus
 
 # create app
 app = Flask(__name__)
@@ -18,17 +19,21 @@ def index():
         return render_template('index.html')
     elif request.method == 'POST':
         # take values from user
+        
         uref = float(request.form.get('uref'))
         rho = float(request.form.get('rho'))
         mu = float(request.form.get('mu'))
         lenght = float(request.form.get('lenght'))
         Y = float(request.form.get('y+'))
         #do calculations
+        '''
         Rex = ((rho*uref*lenght)/mu)
         Cf = ((0.026)/pow(Rex,1/7))
         tauW = ((Cf*rho*uref*uref)/2)
         Ufric = sqrt(tauW/rho)
         distance = ((Y*mu)/(Ufric*rho))
+        '''
+        y_plus(uref, rho, mu, lenght, Y)
         if Rex > 10000:
             Rex = round(Rex, 0)
         else:
