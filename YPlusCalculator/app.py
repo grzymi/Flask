468c39442963e77bf3923335be37c45bbc08a927
grzymi <przemyslaw.grzymislawski@gmail.com>
2019-04-04 23:20:22 +0200

    Make calculation in external function

--- YPlusCalculator/app.py
+++ YPlusCalculator/app.py
@@ -6,7 +6,7 @@
 
 from flask import Flask, request, render_template
 from math import sqrt
-import y_plus
+from y_plus import y_plus
 
 # create app
 app = Flask(__name__)
@@ -25,23 +25,17 @@ def index():
         mu = float(request.form.get('mu'))
         lenght = float(request.form.get('lenght'))
         Y = float(request.form.get('y+'))
-        #do calculations
-        '''
-        Rex = ((rho*uref*lenght)/mu)
-        Cf = ((0.026)/pow(Rex,1/7))
-        tauW = ((Cf*rho*uref*uref)/2)
-        Ufric = sqrt(tauW/rho)
-        distance = ((Y*mu)/(Ufric*rho))
-        '''
-        y_plus(uref, rho, mu, lenght, Y)
+        #do calculations in external function
+        result = y_plus(uref, rho, mu, lenght, Y) #assign returned values
+        Rex, distance = [result[i] for i in [0,1]] #assign values from position
         if Rex > 10000:
             Rex = round(Rex, 0)
         else:
             Rex = round(Rex, 2)
-        #return 'Suma: {0} \nRóżnica: {1}'.format(result1, result2)
         return render_template ('result.html', Rex=Rex, distance=distance)
+
 
 # run app
 if __name__ == '__main__':
-    app.run(host='192.168.0.188', port=5005)
+    app.run(host='192.168.0.188', port=5005, debug=True)
     #app.run(debug=True)
--- YPlusCalculator/y_plus.py
+++ YPlusCalculator/y_plus.py
@@ -1,7 +1,11 @@
+from math import sqrt
+
 def y_plus(uref, rho, mu, lenght, Y):
 	Rex = ((rho*uref*lenght)/mu)
 	Cf = ((0.026)/pow(Rex,1/7))
 	tauW = ((Cf*rho*uref*uref)/2)
 	Ufric = sqrt(tauW/rho)
 	distance = ((Y*mu)/(Ufric*rho))
-	return distance, Rex
+	#nonlocal Rex
+	#nonlocal distance
+	return [Rex, distance]
